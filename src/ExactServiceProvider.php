d45409b67698d7ee8e641490ac3b8c3355c558d2
Vincent van Hoven <vincent@pionect.nl>
2021-08-30 16:44:09 +0200

    [ADD] composer.json, empty service provider

---
+++ src/ExactServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Daalder\Exact;
+
+use Illuminate\Contracts\Container\BindingResolutionException;
+use Illuminate\Support\ServiceProvider;
+
+class ExactServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     * @throws BindingResolutionException
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+}
