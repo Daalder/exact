1933e3a5b4fbc7ba3885fc04a6be91f9eae50414
Vincent van Hoven <vincent@pionect.nl>
2021-09-23 14:59:46 +0200

    [FIX] Authentication issues with multiple simultaneous requests
    Co-Authored-By: Vincent van Hoven <11269255+vincentvanhoven@users.noreply.github.com>

--- src/ServiceProviders/ConnectionServiceProvider.php
+++ src/ServiceProviders/ConnectionServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace Daalder\Exact\ServiceProviders;
 
+use Illuminate\Log\Logger;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\ServiceProvider;
 use Picqer\Financials\Exact\Connection;
 
@@ -12,76 +14,64 @@ class ConnectionServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot() {}
+    public function boot() {
+        $this->app->singleton(Connection::class, function () {
+            // Create Connection instance and setup some config keys
+            $connection = new Connection();
+            $connection->setRedirectUrl(config('daalder-exact.callback_url'));
+            $connection->setExactClientId(config('daalder-exact.client_id'));
+            $connection->setExactClientSecret(config('daalder-exact.client_secret'));
+            $connection->setBaseUrl(config('daalder-exact.base_url'));
 
-    public function register() {
-        try {
-
-            $this->app->singleton(Connection::class, function () {
-                $connection = new Connection();
-                $connection->setRedirectUrl(config('daalder-exact.callback_url'));
-                $connection->setExactClientId(config('daalder-exact.client_id'));
-                $connection->setExactClientSecret(config('daalder-exact.client_secret'));
-                $connection->setBaseUrl(config('daalder-exact.base_url'));
-
-                $file = '';
-                try {
-                    $file = file_get_contents(storage_path('exact/oauth.json'));
-                } catch (\Exception $e) {
-                    //
-                }
-
-                $file = trim(preg_replace('/\s\s+/', ' ', $file));
-                $file = json_decode($file);
-
-                if (optional($file)->authorization_code) {
-                    // Retrieves authorizationcode from database
-                    $connection->setAuthorizationCode($file->authorization_code);
-                }
-
-                if (optional($file)->access_token) {
-                    // Retrieves accesstoken from database
-                    $connection->setAccessToken($file->access_token);
-                }
-
-                if (optional($file)->refresh_token) {
-                    // Retrieves refreshtoken from database
-                    $connection->setRefreshToken($file->refresh_token);
-                }
-
-                if (optional($file)->expires_in) {
-                    // Retrieves expires timestamp from database
-                    $connection->setTokenExpires($file->expires_in);
-                }
-
+            // Get contents of file oauth.json
+            $file = '';
+            try {
+                $file = file_get_contents(storage_path('exact/oauth.json'));
+            } catch (\Exception $e) {
                 //
-                if ($connection->needsAuthentication()) {
-                    return $connection;
-                }
+            }
 
-                // Make the client connect and exchange tokens
-                try {
-                    $connection->connect();
-                } catch (\Exception $e) {
-                    throw new \Exception('Could not connect to Exact: '.$e->getMessage());
-                }
+            // Format data from file
+            $file = trim(preg_replace('/\s\s+/', ' ', $file));
+            $file = json_decode($file);
 
+            // Get codes/tokens from file and set them on the Connection
+            if (optional($file)->authorization_code) {
+                $connection->setAuthorizationCode($file->authorization_code);
+            }
+
+            if (optional($file)->access_token) {
+                $connection->setAccessToken($file->access_token);
+            }
+
+            if (optional($file)->refresh_token) {
+                $connection->setRefreshToken($file->refresh_token);
+            }
+
+            if (optional($file)->expires_in) {
+                $connection->setTokenExpires($file->expires_in);
+            }
+
+            // Set callback on connection for storing newly fetched codes/tokens
+            $connection->setTokenUpdateCallback(function() use ($file, $connection) {
                 $updateOauthFile = false;
-                // Save the new tokens for next connections
+
                 if (optional($file)->access_token !== $connection->getAccessToken()) {
                     $file->access_token = $connection->getAccessToken();
                     $updateOauthFile = true;
                 }
+
                 if (optional($file)->refresh_token !== $connection->getRefreshToken()) {
                     $file->refresh_token = $connection->getRefreshToken();
                     $updateOauthFile = true;
                 }
-                // Optionally, save the expiry-timestamp. This prevents exchanging valid tokens (ie. saves you some requests)
+
                 if (optional($file)->expires_in !== $connection->getTokenExpires()) {
                     $file->expires_in = $connection->getTokenExpires();
                     $updateOauthFile = true;
                 }
 
+                // If any tokens/codes were changed
                 if ($updateOauthFile) {
                     // If storage/exact directory doesn't exist yet
                     if (file_exists(storage_path('exact')) === false) {
@@ -90,11 +80,50 @@ class ConnectionServiceProvider extends ServiceProvider
                     }
                     file_put_contents(storage_path('exact/oauth.json'), json_encode($file));
                 }
-
-                return $connection;
             });
-        } catch (\Exception $e) {
-            //
+
+            // Set callbacks for locking/unlocking the token callback. This prevents multiple simultaneous requests
+            // from messing up the stored tokens.
+            $connection->setAcquireAccessTokenLockCallback($this->lockExactTokenCall);
+            $connection->setAcquireAccessTokenUnlockCallback($this->unlockExactTokenCall);
+
+            try {
+                // Connect and exchange tokens
+                $connection->connect();
+            } catch (\Exception $e) {
+                // Log connection exception
+                Logger()->error('Could not connect to Exact: ' . $e->getMessage());
+            }
+
+            // Always return a Connection, even if it didn't authenticate successfully
+            return $connection;
+        });
+    }
+
+    private function lockExactTokenCall() {
+        // If another thread is currently doing a token request
+        if(cache()->get('exact-lock') === true) {
+            $startTime = now();
+
+            // Wait for the other thread to unlock the exact-lock
+            do {
+                // Wait 100ms before testing the lock again
+                sleep(0.1);
+
+                // If the wait timeout was exceeded
+                if($startTime->diffInSeconds(now()) > 10) {
+                    // Fail this thread/request
+                    throw new \Exception('Exact lock time exceeded');
+                }
+            } while(cache()->get('exact-lock') === true);
         }
+
+        // Lock the exact-lock (because this thread will now do a token request)
+        cache()->set('exact-lock', true);
+    }
+
+    private function unlockExactTokenCall() {
+        // Unlock the exact-lock
+        cache()->set('exact-lock', false);
     }
 }
