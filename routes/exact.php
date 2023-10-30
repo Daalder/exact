21fe849be4d7bfff512e120da27481fc6e233bcf
Vincent van Hoven <vincent@pionect.nl>
2023-10-30 18:43:04 +0100

    [TEST] Github Action
    Co-Authored-By: Vincent van Hoven <11269255+vincentvanhoven@users.noreply.github.com>

--- routes/exact.php
+++ routes/exact.php
@@ -15,3 +15,4 @@ Route::group(['prefix' => 'exact'], function () {
 Route::group(['middleware' => ['api_client']], function () {
     Route::get('authenticate-exact', AuthController::class.'@authenticateExact');
 });
+
